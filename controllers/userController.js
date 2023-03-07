48afd0ca5abf1f8fdc7fbf558b8e340a8a840d07
arnold-O <arnoldoghiator@gmail.com>
2023-03-07 16:39:20 +0100

    code modify

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,47 +1,31 @@
-const User = require('../models/userModel')
-const asyncHandler = require('../utils/asynHandler')
+const User = require("../models/userModel");
+const asyncHandler = require("../utils/asynHandler");
 
+const createUser = asyncHandler(async (req, res, next) => {
+  const { firstname, lastname, email, password, mobile } = req.body;
 
+  const userExist = await User.findOne(email);
 
+  if (!userExist) {
+    // create user
 
+    const user = await User.create({
+      firstname,
+      lastname,
+      email,
+      password,
+      mobile,
+    });
 
-
-const createUser = asyncHandler( async(req, res, next)=>{
-
-
-    const { firstname, lastname, email, password, mobile} = req.body
-
-    const userExist = await User.findOne(email)
-
-
-    if(!userExist){
-
-        // create user
-
-        const user = await User.create({
-
-            firstname,
-            lastname,
-            email,
-            password,
-            mobile
-
-        })
-
-        res.status(200).json({
-            status:"success",
-            user
-        })
-    }else{
-        throw new Error('User already exist')
-
-    }
-}
-
-)
-
-
+    res.status(200).json({
+      status: "success",
+      user,
+    });
+  } else {
+    throw new Error("User already exist");
+  }
+});
 
 module.exports = {
-    createUser
-}
+  createUser,
+};
